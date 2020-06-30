bc234e74ff518fbf7f71bf748df4609e24ebea2e
tostos <saittosun2@gmail.com>
2020-06-30 14:01:12 +0200

    updating data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -28,6 +28,13 @@ export class PostsComponent implements OnInit {
       });
   }
 
+  onUpdatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+      .subscribe(response => {
+        console.log(response)
+      })
+  }
+
   ngOnInit(): void {
   }
 
